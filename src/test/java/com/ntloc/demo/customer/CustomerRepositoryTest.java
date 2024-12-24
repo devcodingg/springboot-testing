6d474e246c4a130f0820740c84372067b664e026
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-24 14:44:35 +0300

    Pass the Test container implementation test

--- src/test/java/com/ntloc/demo/customer/CustomerRepositoryTest.java
+++ src/test/java/com/ntloc/demo/customer/CustomerRepositoryTest.java
@@ -2,17 +2,36 @@ package com.ntloc.demo.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 class CustomerRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.2"));
 
     @Autowired
     CustomerRepository customerRepository;
+
+    @Test
+    void canEstablishDatabaseContainerConnection() {
+        assertTrue(postgreSQLContainer.isCreated());
+        assertTrue(postgreSQLContainer.isRunning());
+    }
 
     @Test
     void shouldReturnCustomerWhenFindByEmail() {
