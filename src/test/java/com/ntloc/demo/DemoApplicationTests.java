07d3daaa4c63c21b1f8847bcc6834995364bafc9
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-24 14:08:08 +0300

    Configure PostgreSQL database via Docker & Reconfigure Java SDK and Maven versions & Fresh test starter

--- src/test/java/com/ntloc/demo/DemoApplicationTests.java
+++ src/test/java/com/ntloc/demo/DemoApplicationTests.java
@@ -1,10 +1,14 @@
 package com.ntloc.demo;
 
-//@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class DemoApplicationTests {
 
-    //    @Test
+    @Test
     void contextLoads() {
+        System.out.println("Tests starter");
     }
 
 }
